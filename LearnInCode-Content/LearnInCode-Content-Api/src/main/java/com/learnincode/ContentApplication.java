830b76229e6f0fa9d09182c2674b414d40b0771a
calmkin <490363906@qq.com>
2024-01-12 22:55:06 +0800

    feat: 内容管理模块——添加接口文档，mock测试分页查询接口

--- LearnInCode-Content/LearnInCode-Content-Api/src/main/java/com/learnincode/ContentApplication.java
+++ LearnInCode-Content/LearnInCode-Content-Api/src/main/java/com/learnincode/ContentApplication.java
@@ -1,9 +1,12 @@
 package com.learnincode;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableSwagger2Doc
 public class ContentApplication {
     public static void main(String[] args) {
         SpringApplication.run(ContentApplication.class);
--- LearnInCode-Content/LearnInCode-Content-Api/src/main/java/com/learnincode/content/api/CourseBaseInfoController.java
+++ LearnInCode-Content/LearnInCode-Content-Api/src/main/java/com/learnincode/content/api/CourseBaseInfoController.java
@@ -7,10 +7,10 @@ import com.learnincode.content.model.dto.QueryCourseParamsDto;
 import com.learnincode.content.model.po.CourseBase;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import java.util.Collections;
+import java.util.List;
 
 
 /**
@@ -21,19 +21,19 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class CourseBaseInfoController {
-
-
     /**
      * @description 课程信息分页查询
      * @author CalmKin
      * @version 1.0
      * @date 2024/1/12 20:52
      */
-    @PostMapping("/list")
-    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody QueryCourseParamsDto queryCourseParamsDto)
+    @PostMapping("/course/list")
+    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParamsDto)
     {
-
-        return null;
+        CourseBase courseBase = new CourseBase();
+        List<CourseBase> courseBases = Collections.singletonList(courseBase);
+        PageResult<CourseBase> result= new PageResult<>(courseBases,1L,1L,3L);
+        return result;
     }
 
 
