b6c43173fa4bfbccf964c453b4ab688845446c8b
calmkin <490363906@qq.com>
2024-01-20 21:39:31 +0800

    init: mybatisX插件生成基本数据

---
+++ LearnInCode-Media/src/main/java/com/learnincode/media/mapper/MediaProcessHistoryMapper.java
@@ -0,0 +1,15 @@
+package com.learnincode.media.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.learnincode.media.po.MediaProcessHistory;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface MediaProcessHistoryMapper extends BaseMapper<MediaProcessHistory> {
+
+}
---
+++ LearnInCode-Media/src/main/java/com/learnincode/media/mapper/MqMessageMapper.java
@@ -0,0 +1,15 @@
+package com.learnincode.media.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.learnincode.media.po.MqMessage;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface MqMessageMapper extends BaseMapper<MqMessage> {
+
+}
