0f2ea1a31c4e1efe368b3f54e9559562c1464ba3
calmkin <490363906@qq.com>
2024-01-20 21:34:39 +0800

    init: 初始化媒资管理模块

---
+++ LearnInCode-Media/src/test/java/com/learnincode/media/MediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnincode.media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
