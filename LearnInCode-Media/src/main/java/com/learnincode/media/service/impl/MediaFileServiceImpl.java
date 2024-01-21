20543fd732eac0fec63b555856ae3290fc97995e
calmkin <490363906@qq.com>
2024-01-21 16:21:14 +0800

    feat: service层抽取上传文件到MinIO方法

--- LearnInCode-Media/src/main/java/com/learnincode/media/service/impl/MediaFileServiceImpl.java
+++ LearnInCode-Media/src/main/java/com/learnincode/media/service/impl/MediaFileServiceImpl.java
@@ -2,27 +2,64 @@ package com.learnincode.media.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.learnincode.base.exception.BusinessException;
 import com.learnincode.base.model.PageParams;
 import com.learnincode.base.model.PageResult;
 import com.learnincode.media.dto.QueryMediaParamsDto;
+import com.learnincode.media.dto.UploadFileParamsDto;
+import com.learnincode.media.dto.UploadFileResultDto;
 import com.learnincode.media.mapper.MediaFilesMapper;
 import com.learnincode.media.po.MediaFiles;
 import com.learnincode.media.service.MediaFileService;
+import com.learnincode.media.utils.FileUtils;
+import io.minio.MinioClient;
+import io.minio.UploadObjectArgs;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.io.File;
+import java.time.LocalDateTime;
 import java.util.List;
 
 
- @Service
+@Service
+@Slf4j
 public class MediaFileServiceImpl implements MediaFileService {
 
   @Autowired
   MediaFilesMapper mediaFilesMapper;
+    @Autowired
+    private MediaFilesMapper mediaFilesMapper;
+
+    @Autowired
+    private MinioClient minioClient;
 
  @Override
  public PageResult<MediaFiles> queryMediaFiles(Long companyId, PageParams pageParams, QueryMediaParamsDto queryMediaParamsDto) {
+    private static final FileUtils FILE_UTILS = FileUtils.getFileUtils();
+
+    // 普通文件桶
+    @Value("${minio.bucket.files}")
+    private String file_bucket;
+
+    // 视频桶
+    @Value("${minio.bucket.videofiles}")
+    private String video_bucket;
+
+
+    /**
+     * @author CalmKin
+     * @description 分页查询媒资信息
+     * @version 1.0
+     * @date 2024/1/21 14:39
+     */
+    @Override
+    public PageResult<MediaFiles> queryMediaFiles(Long companyId, PageParams pageParams, QueryMediaParamsDto queryMediaParamsDto) {
 
   //构建查询条件对象
   LambdaQueryWrapper<MediaFiles> queryWrapper = new LambdaQueryWrapper<>();
@@ -48,4 +85,30 @@ public class MediaFileServiceImpl implements MediaFileService {
   return mediaListResult;
 
  }
+    /**
+     * 将文件上传到MinIO
+     * @param bucket
+     * @param objectName
+     * @param filePath
+     * @param mimeType
+     * @return
+     */
+    public boolean uploadFileToMinIO(String bucket, String objectName,String filePath, String mimeType)
+    {
+        try {
+            UploadObjectArgs uploadObjectArgs = UploadObjectArgs.builder()
+                    .bucket(bucket)   // 指定桶
+                    .object(objectName)    //指定对象名（可以随便取）
+                    .filename(filePath)   // 指定本地文件路径
+                    .contentType(mimeType)  // 指定文件格式
+                    .build();
+            minioClient.uploadObject(uploadObjectArgs);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("文件上传出错,bucket{},objectName{},出错信息{}",bucket,objectName,e.getMessage());
+            return false;
+        }
+        log.error("文件上传成功,bucket{},objectName{},出错信息{}",bucket,objectName);
+        return true;
+    }
 }
