5e7c36c9ba6c4bbacd2196f6c5ea5fa749b21b74
calmkin <490363906@qq.com>
2024-01-22 11:14:04 +0800

    feat: 完成文件分块demo并测试通过

--- LearnInCode-Media/src/main/java/com/learnincode/media/MediaApplication.java
+++ LearnInCode-Media/src/main/java/com/learnincode/media/MediaApplication.java
@@ -1,11 +1,9 @@
 package com.learnincode.media;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.learnincode.media.mapper")
 public class MediaApplication {
 
     public static void main(String[] args) {
--- LearnInCode-Media/src/test/java/com/learnincode/media/MediaApplicationTests.java
+++ LearnInCode-Media/src/test/java/com/learnincode/media/MediaApplicationTests.java
@@ -9,9 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FilterInputStream;
+import java.io.*;
 
 @SpringBootTest
 class MediaApplicationTests {
@@ -101,6 +99,80 @@ class MediaApplicationTests {
         }
 
     }
+
+    /**
+     * @author CalmKin
+     * @description 测试文件分块功能
+     * @version 1.0
+     * @date 2024/1/22 10:48
+     */
+    @Test
+    void testChunck() throws Exception {
+        // 源文件路径
+        String sourceFile = "C:\\Users\\86158\\Desktop\\文件中转站\\点餐.mp4";
+        // 分块文件存放目录
+        String chunkDir = "C:\\Users\\86158\\Desktop\\文件中转站\\chunk\\";
+        File chunkDirFile = new File(chunkDir);
+        // 如果目录不存在，创建新目录
+        if(!chunkDirFile.exists())
+        {
+            chunkDirFile.mkdirs();
+        }
+
+        // 读取源文件
+        RandomAccessFile raf_r = new RandomAccessFile(sourceFile, "r");
+        long length = raf_r.length();
+
+        // 分块大小
+        int chunkSize = 1024 * 1024 * 1;
+        // 分块数量
+        long chunkCount = (length + chunkSize - 1) / chunkSize;
+
+        // 缓冲区，一次只读1KB
+        byte[] buf = new byte[1024];
+
+        for(int i=0; i<chunkCount; i++)
+        {
+            // 先创建对应分块
+            File chunkFile = new File(chunkDir + i );
+            // 如果文件存在，先删除原来的文件
+            if(chunkFile.exists())
+            {
+                chunkDirFile.delete();
+            }
+
+            // 创建文件
+            boolean success = chunkFile.createNewFile();
+            // 文件创建不成功，没必要执行下去
+            if(!success) return;
+
+            // 文件读取指针
+            int size = -1;
+            // 要往分块里面写数据，所以是rw权限
+            RandomAccessFile raf_rw = new RandomAccessFile(chunkFile, "rw");
+            // 源文件没有读完（只要源文件还剩下哪怕1B，都会按照剩下的大小读掉，只有源文件全读完了，再读才会返回-1）
+            while( (size = raf_r.read(buf) ) != -1 )
+            {
+                raf_rw.write(buf,0, size);
+                // 读满了1MB，那么结束当前chunk
+                if(raf_rw.length() >= chunkSize)
+                {
+                    break;
+                }
+            }
+            // 记得关闭文件读取
+            raf_rw.close();
+            System.out.println("完成分块" + i);
+        }
+        // 整个源文件读完了，把读入流关闭
+        raf_r.close();
+    }
+
+
+
+
+
+
 
 
 }
