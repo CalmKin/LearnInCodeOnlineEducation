cb4e2da727b51db8c7678e866a4baa2503816da0
calmkin <490363906@qq.com>
2024-01-29 14:53:14 +0800

    feat: 视频转码模块——任务执行完之后,更新任务状态

--- LearnInCode-Media/src/main/java/com/learnincode/media/service/MediaProcessService.java
+++ LearnInCode-Media/src/main/java/com/learnincode/media/service/MediaProcessService.java
@@ -1,11 +1,22 @@
 package com.learnincode.media.service;
 
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.learnincode.media.po.MediaProcess;
 
 import java.util.List;
 
-public interface MediaProcessService {
+public interface MediaProcessService extends IService<MediaProcess> {
 
     List<MediaProcess> getTask(int executorId,int totalExecutor, int taskCnt);
+
+    /**
+     * 任务执行结束之后，更新任务状态
+     * @param TaskId 任务id
+     * @param status 任务执行状态
+     * @param fileId 任务对应文件id（因为还要写文件表）
+     * @param Url 如果转码成功，会有一个可直接访问的url
+     * @param errMsg 如果转码失败，会有错误信息
+     */
+    void saveStatusAfterProcess(Long TaskId, String status, String fileId,String Url, String errMsg);
 
 }
--- LearnInCode-Media/src/main/java/com/learnincode/media/service/impl/MediaProcessServiceImpl.java
+++ LearnInCode-Media/src/main/java/com/learnincode/media/service/impl/MediaProcessServiceImpl.java
@@ -1,19 +1,68 @@
 package com.learnincode.media.service.impl;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.learnincode.media.mapper.MediaFilesMapper;
+import com.learnincode.media.mapper.MediaProcessHistoryMapper;
 import com.learnincode.media.mapper.MediaProcessMapper;
+import com.learnincode.media.po.MediaFiles;
 import com.learnincode.media.po.MediaProcess;
+import com.learnincode.media.po.MediaProcessHistory;
 import com.learnincode.media.service.MediaProcessService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-public class MediaProcessServiceImpl implements MediaProcessService {
+@Service
+public class MediaProcessServiceImpl extends ServiceImpl<MediaProcessMapper, MediaProcess> implements MediaProcessService {
 
     @Autowired
     private MediaProcessMapper mediaProcessMapper;
 
+    @Autowired
+    private MediaFilesMapper mediaFilesMapper;
+
+    @Autowired
+    private MediaProcessHistoryMapper mediaProcessHistoryMapper;
+
     @Override
     public List<MediaProcess> getTask(int executorId, int totalExecutor, int taskCnt) {
         return mediaProcessMapper.getTaskList(executorId, totalExecutor, taskCnt);
+    }
+
+    @Override
+    @Transactional
+    public void saveStatusAfterProcess(Long TaskId, String status, String fileId, String Url, String errMsg) {
+        // 先找到对应任务
+        MediaProcess task = getById(TaskId);
+        // 健壮性判断
+        if(task == null) return;
+
+        // 如果任务处理失败，更新任务表
+        if(status.equals("3"))
+        {
+            task.setErrormsg(errMsg);
+            task.setFailCount( task.getFailCount() + 1);
+            task.setStatus("3");
+            // todo 如果失败次数超过三次
+            updateById(task);
+            return;
+        }
+
+        // 任务执行成功
+        // 1. 改文件表，设置url
+        MediaFiles mediaFiles = mediaFilesMapper.selectById(fileId);
+        mediaFiles.setUrl(Url);
+        mediaFilesMapper.updateById(mediaFiles);
+        // 2. 改任务表和历史任务表
+        // 2.1 插入历史任务表
+        MediaProcessHistory history = new MediaProcessHistory();
+        BeanUtils.copyProperties(task, history);
+        mediaProcessHistoryMapper.insert(history);
+
+        // 2.2 从任务表中删除
+        removeById(task);
     }
 }
