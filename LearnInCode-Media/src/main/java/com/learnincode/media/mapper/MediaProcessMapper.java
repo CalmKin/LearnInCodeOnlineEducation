5556b93fc1bce60be0858187d23327ed1772e548
calmkin <490363906@qq.com>
2024-01-29 14:38:20 +0800

    feat: 视频转码模块——数据库实现获取任务前的乐观锁

--- LearnInCode-Media/src/main/java/com/learnincode/media/mapper/MediaProcessMapper.java
+++ LearnInCode-Media/src/main/java/com/learnincode/media/mapper/MediaProcessMapper.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.learnincode.media.po.MediaProcess;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -18,4 +19,12 @@ public interface MediaProcessMapper extends BaseMapper<MediaProcess> {
      */
     @Select("select * from xcplus_media.media_process t where t.id%#{totalExecutor} = #{executorId} and ( t.status = '1' or (t.status = '3' and t.fail_count < 3 ) ) limit #{taskCnt}")
     List<MediaProcess> getTaskList(@Param("executorId") int executorId,@Param("totalExecutor") int totalExecutor,@Param("taskCnt") int taskCnt);
+
+    /**
+     * 利用数据库实现乐观锁,执行任务前先获取乐观锁
+     * @param id
+     * @return
+     */
+    @Update("update xcplus_media.media_process t set t.status = 4 where t.id = #{id} and (t.status = '1' or (t.status = '3' and t.fail_count < 3 ) )")
+    boolean tryLock(@Param("id") long id);
 }
