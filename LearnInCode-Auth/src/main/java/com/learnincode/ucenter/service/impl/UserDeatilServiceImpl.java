c8e3992d29d8e0835410bf3aea49127e8fa40613
calmkin <490363906@qq.com>
2024-02-02 17:44:29 +0800

    feat: 用户认证——将用户信息压缩成JSON保存在UserDetailsService的userName属性中

--- LearnInCode-Auth/src/main/java/com/learnincode/ucenter/service/impl/UserDeatilServiceImpl.java
+++ LearnInCode-Auth/src/main/java/com/learnincode/ucenter/service/impl/UserDeatilServiceImpl.java
@@ -1,5 +1,6 @@
 package com.learnincode.ucenter.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.learnincode.ucenter.mapper.UserMapper;
 import com.learnincode.ucenter.model.po.User;
@@ -16,7 +17,7 @@ public class UserDeatilServiceImpl implements UserDetailsService {
 
     /**
      * @author CalmKin
-     * @description 根据账号查询用户信息,并封装成UserDetails返回
+     * @description 根据账号查询用户信息, 并封装成UserDetails返回
      * @version 1.0
      * @date 2024/2/2 16:49
      */
@@ -25,17 +26,30 @@ public class UserDeatilServiceImpl implements UserDetailsService {
         User user = userMapper.selectOne(new LambdaQueryWrapper<User>().eq(User::getUsername, userName));
 
         //返回空表示用户不存在
-        if(user == null) return null;
+        if (user == null) return null;
 
         //取出数据库存储的正确密码
         String password = user.getPassword();
 
         //用户权限,如果不加报Cannot pass a null GrantedAuthority collection
-        String[] authorities= {"test"};
+        String[] authorities = {"test"};
 
-        UserDetails userDetails = org.springframework.security.core.userdetails.User.withUsername(user.getUsername())
+        // 排除敏感信息
+        excludeSensitiveInfo(user);
+        // 将用户信息压缩成JSON串存在UserName中
+        String userInfo = JSON.toJSONString(user);
+
+        UserDetails userDetails = org.springframework.security.core.userdetails.User.withUsername(userInfo)
                 .password(password).authorities(authorities).build();
 
         return userDetails;
     }
+
+    void excludeSensitiveInfo(User user) {
+        user.setPassword(null);
+        user.setEmail(null);
+        user.setQq(null);
+        user.setCellphone(null);
+    }
+
 }
