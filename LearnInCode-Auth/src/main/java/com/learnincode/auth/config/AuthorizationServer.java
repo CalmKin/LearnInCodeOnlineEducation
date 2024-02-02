654d56fb4b9e3b24eedeada9ec5fce12bc1f8786
calmkin <490363906@qq.com>
2024-02-02 12:39:12 +0800

    perf: 认证授权——修改令牌配置类,将普通令牌修改成JWT令牌

--- LearnInCode-Auth/src/main/java/com/learnincode/auth/config/AuthorizationServer.java
+++ LearnInCode-Auth/src/main/java/com/learnincode/auth/config/AuthorizationServer.java
@@ -31,15 +31,15 @@ import javax.annotation.Resource;
   public void configure(ClientDetailsServiceConfigurer clients)
           throws Exception {
         clients.inMemory()// 使用in-memory存储
-                .withClient("XcWebApp")// client_id
-                .secret("XcWebApp")//客户端密钥
+                .withClient("WebApp")// client_id
+                .secret("WebApp")//客户端密钥
 //                .secret(new BCryptPasswordEncoder().encode("XcWebApp"))//客户端密钥
-                .resourceIds("xuecheng-plus")//资源列表
+                .resourceIds("learn-in-code")//资源列表
                 .authorizedGrantTypes("authorization_code", "password","client_credentials","implicit","refresh_token")// 该client允许的授权类型authorization_code,password,refresh_token,implicit,client_credentials
                 .scopes("all")// 允许的授权范围
                 .autoApprove(false)//false跳转到授权页面
                 //客户端接收授权码的重定向地址
-                .redirectUris("http://www.51xuecheng.cn")
+                .redirectUris("http://www.learnincode.cn")
    ;
   }
 
