f9e2d7832dd18ea867a737bb5eef060f1082b8b4
calmkin <490363906@qq.com>
2024-02-03 21:57:06 +0800

    feat: 选课学习模块——新建订单模块,导入基本实体类、工具类、配置类、maven依赖

---
+++ LearnInCode-Orders/src/main/java/com/learnincode/orders/mapper/OrdersGoodsMapper.java
@@ -0,0 +1,9 @@
+package com.learnincode.orders.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.learnincode.orders.model.po.OrdersGoods;
+
+
+public interface OrdersGoodsMapper extends BaseMapper<OrdersGoods> {
+
+}
