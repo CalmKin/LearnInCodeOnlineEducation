0e2db98de256cc1b7491e99ec48d5d81ce524a75
calmkin <490363906@qq.com>
2024-02-22 22:03:02 +0800

    chore: 第三方认证授权——使用OkHttp3作为Http请求工具

--- LearnInCode-Auth/src/main/java/com/learnincode/AuthApplication.java
+++ LearnInCode-Auth/src/main/java/com/learnincode/AuthApplication.java
@@ -3,10 +3,20 @@ package com.learnincode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages={"com.learnincode.*.feignclient"})
 public class AuthApplication {
+
+    // 注入rest请求工具Bean
+    @Bean
+    RestTemplate restTemplate(){
+        RestTemplate restTemplate = new RestTemplate(new OkHttp3ClientHttpRequestFactory());
+        return  restTemplate;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
